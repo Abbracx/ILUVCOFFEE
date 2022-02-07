5fbbc01cfaaed4c1aae85405ea693f01375077fa
Abbracx <tankoraphael@gmail.com>
2022-02-07 12:48:48 +0100

    Created a typeORM Entity for coffee and registered it in coffee child module

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,6 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
+import { Coffee } from './entities/coffee.entity';
 
-@Module({ controllers: [CoffeesController], providers: [CoffeesService] })
+
+//The use of forFeature() registers TypeORM in this child module
+@Module({
+  imports: [TypeOrmModule.forFeature([Coffee])],
+  controllers: [CoffeesController],
+  providers: [CoffeesService],
+})
 export class CoffeesModule {}
--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,18 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity() //sql table === 'coffee' 
 export class Coffee {
+  @PrimaryGeneratedColumn() // defines id as primary column and also autoincrement it for us.
   id: number;
+
+  @Column()
   name: string;
+
+  @Column()
   brand: string;
+
+  @Column('json', { nullable: true }) // flavors stores arrays as json and makes the column optional
   flavors: string[];
 }
+
+//We need to make TypeORM aware of the entity inside of these child module itself
