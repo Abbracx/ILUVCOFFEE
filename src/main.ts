b7fa59f2ba7fd1a517047523d527cb5dd6adff4c
Abbracx <tankoraphael@gmail.com>
2022-02-07 03:14:46 +0100

    Enabled autotransform for payloads

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // to avoid users from passing invalid data to our controller
+      transform: true, //Making sure our type from post request is what is expected also applicable for primitive types
+      forbidNonWhitelisted: true, //
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
